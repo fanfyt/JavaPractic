f649d5f55bd5d0dbdab00ef1dbd0aca6ae08c2d8
fl <fanfyt@163.com>
2022-07-05 01:06:35 +0800

    2.

--- src/main/java/com/liang/leecode/_2两数相加/Answer3.java
+++ src/main/java/com/liang/leecode/_2两数相加/Answer3.java
@@ -10,13 +10,13 @@ import org.junit.jupiter.api.Test;
  **/
 public class Answer3 {
     @Test
-    public void test() {
+    public void Answer3test() {
 
-        int a[] = {1, 3, 4};
-        int b[] = {1, 2};
+        int[] a = {1, 3, 4};
+        int[] b = {1, 2};
 
-        int[] wrong1 = {9,9,9,9,9,9,9};
-        int[] wrong2 = {9,9,9,9};
+        int[] wrong1 = {9, 9, 9, 9, 9, 9, 9};
+        int[] wrong2 = {9, 9, 9, 9};
         ListNode listNode1 = ListNodeGenerator.getListNode(wrong1);
         System.out.println("  ---   ");
         ListNode listNode2 = ListNodeGenerator.getListNode(wrong2);
@@ -25,7 +25,7 @@ public class Answer3 {
 
         System.out.println(" 結果 ：");
         while (listNode != null) {
-            System.out.println(listNode.val);
+            System.out.print(listNode.val);
 
             if (listNode.next != null) {
                 listNode = listNode.next;
@@ -52,30 +52,26 @@ public class Answer3 {
                 over = sum / 10;
                 add = sum % 10;
                 l2 = l2.next;
-                l3.val = add;
             } else if (l2Null) {
                 // l1.next 不为null
                 sum = l1.val + over;
                 over = sum / 10;
                 add = sum % 10;
                 l1 = l1.next;
-                l3.val = add;
             } else {
                 // l1、l2 next都不为null
                 sum = l1.val + l2.val + over;
                 over = sum / 10;
                 add = sum % 10;
-
                 l1 = l1.next;
                 l2 = l2.next;
-                l3.val = add;
             }
-
-            if(l1 == null && l2 == null && over >0){
+            l3.val = add;
+            if (l1 == null && l2 == null && over > 0) {
                 l3 = new ListNode(over, l3);
-            }else if(l1 == null && l2 == null){
+            } else if (l1 == null && l2 == null) {
                 break;
-            }else {
+            } else {
                 l3 = new ListNode(0, l3);
             }
 
